a2eecc15c8f740bb4f7e0e5df0f07c758230e79c
mathewalphons <mathewalphons1982@gmail.com>
2021-02-15 14:35:53 +0400

    redux-in-productdetails

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,20 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
-import thunk from 'redux-thunk'
-
-import { productListReducer } from './reducers/productReducers';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
-    productList : productListReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
- const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
- const store = createStore(reducer, 
-    initialState,
-     composeEnhancer(applyMiddleware(thunk)));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
- export default store;
-
+export default store;
