414596e3b5871db3c68e9e996e391516925ebd4a
mathewalphons <mathewalphons1982@gmail.com>
2021-02-19 00:03:53 +0400

    delete cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,6 @@
   
 import Axios from 'axios';
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   const { data } = await Axios.get(`/api/products/${productId}`);
@@ -14,6 +14,14 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
       product: data._id,
       qty,
     },
+  });
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => async (dispatch, getState) =>{
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload:productId    
   });
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
