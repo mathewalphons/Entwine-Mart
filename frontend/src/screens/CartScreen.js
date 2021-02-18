1c376fd1b285f932435940bca8883a4071aca5c5
mathewalphons <mathewalphons1982@gmail.com>
2021-02-18 14:33:34 +0400

    cart-build

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function CartScreen(props) {
+    const productId = props.match.params.id;
+    const qty = props.location.search 
+    ? Number(props.location.search.split('=')[1]): 1;
+    return (
+        <div>
+            <h1>Cart Screen</h1>
+            <p>
+                Add to Cart : Product Id: {productId} Qty: {qty}
+            </p>
+        </div>
+    )
+}
